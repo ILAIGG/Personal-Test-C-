2e215ed0281e4757dbffda1f8b0f25a0519e3fba
Lucas <lucasmordka9@gmail.com>
2026-03-16 19:05:22 -0300

    Debug setted

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -5,6 +5,13 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        Console.WriteLine("Hola Mundo!");
+    }
 
+    [Fact]
+    public int Numerin()
+    {
+        int numerin = 9;
+        return numerin;
     }
 }
